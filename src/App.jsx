ee204e381de59003d4409fbf0070ca8e94fa2971
mehedipolash <mehedipolash228@gmail.com>
2025-07-02 23:58:07 +0600

    project setup done and some basic navbar type created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import DaisyNav from './Components/DaisyNav/DaisyNav';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navbar></Navbar>
+        <DaisyNav></DaisyNav>
+      </header>
+
+      <main></main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from './Link';
+
+const navigationData = [
+  { id: 1, name: 'Home', path: '/' },
+  { id: 2, name: 'About', path: '/about' },
+  { id: 3, name: 'Services', path: '/services' },
+  { id: 4, name: 'Blog', path: '/blog' },
+  { id: 5, name: 'Contact', path: '/contact' },
+];
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="flex">
+        {navigationData.map(route => (
+          <Link route={route} key={route.id}></Link>
+        ))}
+      </ul>
+
+      {/* <ul className="flex">
+        {navigationData.map(route => (
+          <li className="mr-10">
+            <a href={route.path}>{route.name}</a>
+          </li>
+        ))}
+      </ul> */}
+
+      {/* <ul>
+        <li className="mr-10">
+          <a href="/">Home</a>
+        </li>
+        <li className="mr-10">
+          <a href="/about">About</a>
+        </li>
+        <li className="mr-10">
+          <a href="/blog">Blog</a>
+        </li>
+      </ul> */}
+    </nav>
+  );
+};
+
+export default Navbar;
